5e7b0291f06a59b2ad54994c37f1b68541981b12
slevine3 <levinesam3@gmail.com>
2022-06-01 22:35:49 +0300

    responsive design change and debugging

--- client/requestMethods.js
+++ client/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://daily-task-scheduler.herokuapp.com/api";
 
 export const publicRequest = axios.create({ baseURL: BASE_URL });
