4674d13eedb20e95ec85da4ca21d0420aeb5854e
samlevine3 <sam@boaideas.com>
2024-06-05 09:11:06 +0300

    remove log rocket

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useEffect } from "react";
 import Home from "./pages/Home/Home";
 import RescheduleTask from "./pages/RescheduleTask/RescheduleTask";
-import LogRocket from "logrocket";
+// import LogRocket from "logrocket";
 import Schedule from "./pages/Schedule/Schedule";
 import RescheduleRecurring from "./pages/RescheduleRecurring/RescheduleRecurring";
 
 const App = () => {
-  useEffect(() => {
-    LogRocket.init(process.env.REACT_APP_LOG_ROCKET);
-  });
+  // useEffect(() => {
+  //   LogRocket.init(process.env.REACT_APP_LOG_ROCKET);
+  // });
 
   return (
     <div>
